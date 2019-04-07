e869b2008f3807b5fc4fbab6b08b2204940a3e56
Dan Schulte <dan.schulte.qub@live.com>
2019-04-07 14:15:07 -0700

    Still show test coverage even if tests fail

--- sources/qub/RealJavaRunner.java
+++ sources/qub/RealJavaRunner.java
@@ -5,88 +5,83 @@ public class RealJavaRunner extends JavaRunner
     @Override
     public Result<Integer> run(Console console)
     {
-        final ProcessBuilder javaExe = console.getProcessBuilder("java.exe").await();
-        javaExe.redirectOutput(console.getOutputByteWriteStream());
-        javaExe.redirectError(console.getErrorByteWriteStream());
-
-        final Folder outputFolder = getOutputFolder();
-
-        final Folder jacocoFolder = getJacocoFolder();
-        if (jacocoFolder != null)
+        return Result.create(() ->
         {
-            javaExe.addArgument("-javaagent:" + getJacocoAgentJarFile().toString() + "=destfile=" + getCoverageExecFile().toString());
-        }
+            final ProcessBuilder javaExe = console.getProcessBuilder("java.exe").await();
+            javaExe.redirectOutput(console.getOutputByteWriteStream());
+            javaExe.redirectError(console.getErrorByteWriteStream());
 
-        javaExe.addArguments("-classpath", getClassPath());
+            final Folder outputFolder = getOutputFolder();
 
-        javaExe.addArgument("qub.ConsoleTestRunner");
-
-        javaExe.addArguments(getFullClassNames());
-
-        final String pattern = getPattern();
-        if (!Strings.isNullOrEmpty(pattern))
-        {
-            javaExe.addArgument("-pattern=" + pattern);
-        }
-
-        final boolean isVerbose = QubTest.isVerbose(console);
-        if (isVerbose)
-        {
-            QubTest.verbose(console, javaExe.getCommand());
-        }
-
-        console.writeLine().await();
-
-        return javaExe.run()
-            .then((Integer exitCode) ->
+            final Folder jacocoFolder = getJacocoFolder();
+            if (jacocoFolder != null)
             {
-                Integer result;
-                if (exitCode == null || exitCode != 0 || jacocoFolder == null)
-                {
-                    result = exitCode;
-                }
-                else
-                {
-                    console.writeLine().await();
-                    console.writeLine("Analyzing coverage...").await();
+                javaExe.addArgument("-javaagent:" + getJacocoAgentJarFile().toString() + "=destfile=" + getCoverageExecFile().toString());
+            }
 
-                    final File jacocoCLIJarFile = jacocoFolder.getFile("jacococli.jar").await();
-                    final Folder coverageFolder = outputFolder.getFolder("coverage").await();
+            javaExe.addArguments("-classpath", getClassPath());
 
-                    final ProcessBuilder jacococli = console.getProcessBuilder("java").awaitError();
-                    jacococli.addArguments("-jar", jacocoCLIJarFile.toString());
-                    jacococli.addArgument("report");
-                    jacococli.addArgument(getCoverageExecFile().toString());
+            javaExe.addArgument("qub.ConsoleTestRunner");
 
-                    final Path currentFolderPath = console.getCurrentFolderPath();
-                    final Iterable<File> classFiles = getClassFiles();
-                    for (final File classFile : classFiles)
-                    {
-                        jacococli.addArguments("--classfiles", classFile.relativeTo(currentFolderPath).toString());
-                    }
-                    jacococli.addArguments("--sourcefiles", getSourceFolder().toString());
-                    jacococli.addArguments("--html", coverageFolder.toString());
+            javaExe.addArguments(getFullClassNames());
 
-                    if (isVerbose)
-                    {
-                        console.writeLine().await();
-                        QubTest.verbose(console, jacococli.getCommand());
-                        jacococli.redirectOutput(console.getOutputByteWriteStream());
-                        jacococli.redirectError(console.getErrorByteWriteStream());
-                    }
-                    result = jacococli.run().await();
+            final String pattern = getPattern();
+            if (!Strings.isNullOrEmpty(pattern))
+            {
+                javaExe.addArgument("-pattern=" + pattern);
+            }
 
-                    final File coverageHtmlFile = coverageFolder.getFile("index.html").await();
-                    try
-                    {
-                        java.awt.Desktop.getDesktop().open(new java.io.File(coverageHtmlFile.toString()));
-                    }
-                    catch (java.io.IOException e)
-                    {
-                        e.printStackTrace();
-                    }
-                }
-                return result;
-            });
+            final boolean isVerbose = QubTest.isVerbose(console);
+            if (isVerbose)
+            {
+                QubTest.verbose(console, javaExe.getCommand());
+            }
+
+            console.writeLine().await();
+
+            final Integer testsExitCode = javaExe.run().await();
+
+            console.writeLine().await();
+            console.writeLine("Analyzing coverage...").await();
+
+            final File jacocoCLIJarFile = jacocoFolder.getFile("jacococli.jar").await();
+            final Folder coverageFolder = outputFolder.getFolder("coverage").await();
+
+            final ProcessBuilder jacococli = console.getProcessBuilder("java").await();
+            jacococli.addArguments("-jar", jacocoCLIJarFile.toString());
+            jacococli.addArgument("report");
+            jacococli.addArgument(getCoverageExecFile().toString());
+
+            final Path currentFolderPath = console.getCurrentFolderPath();
+            final Iterable<File> classFiles = getClassFiles();
+            for (final File classFile : classFiles)
+            {
+                jacococli.addArguments("--classfiles", classFile.relativeTo(currentFolderPath).toString());
+            }
+            jacococli.addArguments("--sourcefiles", getSourceFolder().toString());
+            jacococli.addArguments("--html", coverageFolder.toString());
+
+            if (isVerbose)
+            {
+                console.writeLine().await();
+                QubTest.verbose(console, jacococli.getCommand());
+                jacococli.redirectOutput(console.getOutputByteWriteStream());
+                jacococli.redirectError(console.getErrorByteWriteStream());
+            }
+
+            final Integer coverageExitCode = jacococli.run().await();
+
+            final File coverageHtmlFile = coverageFolder.getFile("index.html").await();
+            try
+            {
+                java.awt.Desktop.getDesktop().open(new java.io.File(coverageHtmlFile.toString()));
+            }
+            catch (java.io.IOException e)
+            {
+                e.printStackTrace();
+            }
+
+            return testsExitCode != 0 ? testsExitCode : coverageExitCode;
+        });
     }
 }
