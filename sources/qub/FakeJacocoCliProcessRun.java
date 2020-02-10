e099a692b23fb1c88f1105154d4dc74ec0dc8289
Dan Schulte <dan.schulte.qub@live.com>
2020-02-10 08:19:25 -0800

    Extract qub/jacococli-java from qub/test-java

--- sources/qub/FakeJacocoCliProcessRun.java
+++
@@ -1,9 +0,0 @@
-package qub;
-
-public class FakeJacocoCliProcessRun extends FakeProcessRunDecorator<FakeJacocoCliProcessRun> implements JavaArguments<FakeJacocoCliProcessRun>, JacocoCliArguments<FakeJacocoCliProcessRun>
-{
-    public FakeJacocoCliProcessRun()
-    {
-        super(new BasicFakeProcessRun(JavaProcessBuilder.executablePath));
-    }
-}
